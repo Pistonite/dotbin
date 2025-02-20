c51c272f1018ebe8108f07359eb27e828d94ff9f
Michael Zhao <44533763+Pistonight@users.noreply.github.com>
2025-02-19 16:44:29 -0800

    fix wsclip for asyncio in python 3.13

--- wsclip.py
+++ wsclip.py
@@ -32,12 +32,12 @@ This requires `export HOST_MACHINE_IP=<IP>` in your `~/.bashrc` and `cargo insta
 """
 
 import asyncio
-import websockets
+from websockets.asyncio.server import serve
 import sys
 import pyperclip
 DEFAULT_PORT = 8881
 
-def run_server(port):
+async def run_server(port):
     async def handle_message(websocket):
         async for message in websocket:
             # Replace the null character from vim yank with newline
@@ -45,16 +45,16 @@ def run_server(port):
             length = len(message)
             print(f"Received message {length=}")
             pyperclip.copy(message)
-    start_server = websockets.serve(handle_message, "0.0.0.0", port)
-    asyncio.get_event_loop().run_until_complete(start_server)
-    print(f"Server started on port {port}")
-    try:
-        asyncio.get_event_loop().run_forever()
-    except KeyboardInterrupt:
-        print("Server stopped.")
+
+    print(f"starting server on port {port}")
+    async with serve(handle_message, "0.0.0.0", port) as server:
+        await server.serve_forever()
 
 if __name__ == "__main__":
     port = DEFAULT_PORT
     if len(sys.argv) > 1:
         port = int(sys.argv[1])
-    run_server(port)
+    try:
+        asyncio.run(run_server(port))
+    except KeyboardInterrupt:
+        print("server stopped")
